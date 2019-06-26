d15002f67ae7345e664a585fcb356198ccbe8920
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-26 23:29:25 +0900

    add spa

--- static/src/App.svelte
+++ static/src/App.svelte
@@ -1,7 +1,22 @@
 <script>
-    let word = "Hello World!"
+    import { Router, Link, Route } from "svelte-routing"
+    import index from "./routes/index.svelte"
+    import about from "./routes/about.svelte"
+    import contact from "./routes/contact.svelte"
+
+    export let url = ""
 </script>
 
-<div data-controller="hello">
-    <h1>{ word }</h1>
-</div>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Index</Link>
+    <Link to="about">About</Link>
+    <Link to="contact">Contact</Link>
+  </nav>
+  <div>
+    <Route path="/" component="{index}" />
+    <Route path="/about" component="{about}" />
+    <Route path="/cotact" component="{contact}" />
+    <Route path="/"><index /></Route>
+  </div>
+</Router>
